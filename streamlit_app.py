7ff580e71f470a6b9f87c48c2be7f275e47499d8
kalligas <123652707+kalligas@users.noreply.github.com>
2023-05-01 13:15:31 +0300

    Update streamlit_app.py
    added missing parenthesis

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,6 @@ my_fruit_list = myfruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page
-streamlit.dataframe(my_fruit_list
+streamlit.dataframe(my_fruit_list)
                     
 
